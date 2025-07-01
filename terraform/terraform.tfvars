11ec011ce18af8f8a53a89d398a5f0466fadedb5
Daniel <daniel@Daniels-MacBook-Pro.local>
2025-07-01 17:12:45 +0100

    add initial test files

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,1 @@
+bucket_name = "my-demo-local-bucket"
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type    = string
+  default = "my-local-bucket"
+}
+
+variable "lambda_name" {
+  type    = string
+  default = "my-local-lambda"
+}
